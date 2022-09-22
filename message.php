c820af4f6d7216ac9485327af7696f7943b419bc
Wesley Marques <101737823+WesleyMarques329@users.noreply.github.com>
2022-09-22 00:52:07 -0300

    Add files via upload

---
+++ message.php
@@ -0,0 +1,24 @@
+<?php
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $phone = htmlspecialchars($_POST['phone']);
+  $website = htmlspecialchars($_POST['website']);
+  $message = htmlspecialchars($_POST['message']);
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Sua mensagem foi enviada";
+      }else{
+         echo "Desculpe, ocorreu um erro ao enviar sua mensagem!";
+      }
+    }else{
+      echo "Entre com um email válido";
+    }
+  }else{
+    echo "Preencha seu email e o campo de mensagem!";
+  }
+?>
