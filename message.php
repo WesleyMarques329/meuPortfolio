a5725482fed5e3353c638a842c0095fa8f27fdbb
Wesley Marques <101737823+WesleyMarques329@users.noreply.github.com>
2022-09-22 09:42:12 -0300

    Update message.php

--- message.php
+++ message.php
@@ -1,12 +1,12 @@
 <?php
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['phone']);
-  $website = htmlspecialchars($_POST['website']);
-  $message = htmlspecialchars($_POST['message']);
+  $name = ($_POST['name']);
+  $email = ($_POST['email']);
+  $phone = ($_POST['phone']);
+  $website = ($_POST['website']);
+  $message = ($_POST['message']);
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
+      $receiver = "wesleymarquees329@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
